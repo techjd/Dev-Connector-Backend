69b3ea960785f28fa9e3e8f63c58ed1fa3568657
Jaydeep <jaydeepparmar253@gmail.com>
2022-02-12 03:41:18 +0530

    Added Changes for Heroku

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/api/jobs', require('./routes/api/jobs'));
 //   });
 // }
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/api/getMe', (req, res) => {
   console.log('Req');
